ec9eeb7f71224dcaa5f8a7c05b771ff6e817d122
ThusoTR <67108860+ThusoTR@users.noreply.github.com>
2020-07-26 00:15:41 +0200

    Url templatting tagging not working for location shortcuts

--- SA_Stats_Visualizer/main_application/stats_picture_names.py
+++ SA_Stats_Visualizer/main_application/stats_picture_names.py
@@ -1,5 +1,18 @@
 #South Africa Stats: Picture provincial_unemploment_trends
+def province_names():
+    province_names = {
+        'Gauteng': 'Gauteng',
+        'Eastern_Cape': 'Eastern Cape',
+        'Kwazulu-Natal': 'Kwazulu-Natal',
+        'Limpopo': 'Limpopo',
+        'Free State': 'Free State',
+        'Western Cape': 'Western Cape',
+        'Northern Cape': 'Northern Cape',
+        'Mpumalanga': 'Mpumalanga',
+        'North West': 'North West'
 
+    }
+    return province_names
 def SA_PICTURES_AND_STATS_VALUES():
     South_Africa_Stats_Dict = {
         'Assumption': "main_application/Population Stats/Assumption of life expectancy.png",
@@ -19,6 +32,73 @@ def SA_PICTURES_AND_STATS_VALUES():
         'Unemployment_by_age_group_Expanded': 'main_application/Economic Stats/Unemployment by age group_Expanded.png',
         'Unemployment_per_population_group': 'main_application/Economic Stats/Unemployment per population group_Expanded.png',
         'Unemployment_per_province_Expanded': 'main_application/Economic Stats/Unemployment per province_Expanded.png',
-
-    }
+        }
     return South_Africa_Stats_Dict;
+
+def PROVINCIAL_PICTURES_AND_STATS_VALUES(province_name):
+    Eastern_Stats_Dict = {
+        'population_by_sex_and_age': "main_application/Economic Stats/Eastern Cape female _Trends.png",
+        'population_trends': "main_application/Economic Stats/Eastern Cape population trend (2002 to 2020)_Trends.png",
+        'unemployment_trends': "main_application/Economic Stats/Eastern Cape_unemployment_trends.png"
+    }
+    Free_State_Stats_Dict = {
+        'population_by_sex_and_age': "main_application/Economic Stats/Free State Cape female _Trends.png",
+        'population_trends': "main_application/Economic Stats/Free State population trend (2002 to 2020)_Trends.png",
+        'unemployment_trends': "main_application/Economic Stats/Frees State_unemployment_trends.png"
+    }
+    Gauteng_Stats_Dict = {
+        'population_by_sex_and_age':"main_application/Economic Stats/Gauteng female _Trends.png",
+        'population_trends':"main_application/Economic Stats/Gauteng population trend (2002 to 2020)_Trends.png",
+        'unemployment_trends': "main_application/Economic Stats/Gauteng_unemployment_trends.png"
+    }
+    KwaZulu_Natal_Stats_Dict = {
+        'population_by_sex_and_age': "main_application/Economic Stats/Kwazulu-Natal_Trends.png",
+        'population_trends': "main_application/Economic Stats/Kwazulu-Natal population trend (2002 to 2020)_Trends.png",
+        'unemployment_trends': "main_application/Economic Stats/Kwazulu-Natal_unemployment_trends.png"
+    }
+    Limpopo_Stats_Dict = {
+        'population_by_sex_and_age': "main_application/Economic Stats/Limpopo female _Trends.png",
+        'population_trends': "main_application/Economic Stats/Limpopo population trend (2002 to 2020)_Trends.png",
+        'unemployment_trends': "main_application/Economic Stats/Limpopo_unemployment_trends.png"
+    }
+    Mpumalanga_Stats_Dict = {
+        'population_by_sex_and_age': "main_application/Economic Stats/Mpumalanga female _Trends.png",
+        'population_trends': "Mpulamanga population trend (2002 to 2020)_Trends.png",
+        'unemployment_trends': "main_application/Economic Stats/Mpumalanga_unemployment_trends.png"
+    }
+
+    North_West_Stats_Dict = {
+        'population_by_sex_and_age': "main_application/Economic Stats/North West female _Trends.png",
+        'population_trends': "main_application/Economic Stats/North West population trend (2002 to 2020)_Trends.png",
+        'unemployment_trends': "main_application/Economic Stats/North West_unemployment_trends.png"
+    }
+    Northern_Cape_Stats_Dict = {
+        'population_by_sex_and_age': "main_application/Economic Stats/Northern Cape female _Trends.png",
+        'population_trends': "main_application/Economic Stats/Northern Cape population trend (2002 to 2020)_Trends.png",
+        'unemployment_trends': "main_application/Economic Stats/Northern Cape_unemployment_trends.png"
+    }
+    Western_Cape_Stats_Dict = {
+        'population_by_sex_and_age': "main_application/Economic Stats/Western Cape female _Trends.png",
+        'population_trends': "main_application/Economic Stats/Western Cape population trend (2002 to 2020)_Trends.png",
+        'unemployment_trends': "main_application/Economic Stats/Western Cape_unemployment_trends.png"
+    }
+    province_name = province_name.upper()
+
+    if (province_name == 'GAUTENG'):
+        return Gauteng_Stats_Dict
+    elif(province_name == 'EASTERN CAPE'):
+        return Eastern_Stats_Dict
+    elif(province_name == 'FREE STATE'):
+        return Free_State_Stats_Dict
+    elif(province_name == 'MPUMALANGA'):
+        return Mpumalanga_Stats_Dict
+    elif(province_name == 'WESTERN CAPE'):
+        return Western_Cape_Stats_Dict
+    elif(province_name == 'KWAZULU-NATAL'):
+        return KwaZulu_Natal_Stats_Dict
+    elif(province_name == 'NORTHERN CAPE'):
+        return Northern_Cape_Stats_Dict
+    elif(province_name == 'LIMPOPO'):
+        return Limpopo_Stats_Dict
+    elif(province_name == 'NORTH WEST'):
+        return North_West_Stats_Dict
--- SA_Stats_Visualizer/main_application/views.py
+++ SA_Stats_Visualizer/main_application/views.py
@@ -2,9 +2,18 @@ from django.shortcuts import render
 from main_application import stats_picture_names
 # Create your views here.
 def home_page(request):
-    return render(request, 'index.html')
+    names_of_provinces = stats_picture_names.province_names()
+    return render(request, 'index.html', names_of_provinces)
 
-def load_south_african_stats(request):
-    states_pictures = stats_picture_names.SA_PICTURES_AND_STATS_VALUES()
-
-    return render(request, 'view_SA_stats.html', states_pictures)
+def load_south_african_stats(request, province_name):
+    stats_pictures = stats_picture_names.SA_PICTURES_AND_STATS_VALUES()
+    names_of_provinces = stats_picture_names.province_names()
+    return render(request, 'view_SA_stats.html', [stats_pictures, names_of_provinces])
+def loaf_provincial_stats(request, **kwargs):
+    province_stats_values_dict = {
+        'province_name': 'province_name'
+    }
+    stats_pictures = stats_picture_names.SA_PICTURES_AND_STATS_VALUES()
+    provincial_stats_pictures = stats_picture_names.SA_PICTURES_AND_STATS_VALUES()
+    return render(request, 'view_provincial_stats.html', [province_stats_values_dict, stats_pictures,
+                                                          provincial_stats_pictures])
